d84b040537606c5f87e19ddf10a9dbfc4a282fb3
geoyogesh <geoyogesh@gmail.com>
2014-08-01 22:15:32 -0500

    File to feature class conversion

--- sample.py
+++ sample.py
@@ -2,12 +2,12 @@ __author__ = 'Yogesh'
 
 
 import arcpy
-import zipfile
-import os.path
-import shutil
+import os
+
 # Function to unzipping the contents of the zip file
 #
 def unzip(source_filename, dest_dir):
+    import zipfile,os.path
     with zipfile.ZipFile(source_filename) as zf:
         for member in zf.infolist():
             # Path traversal defense copied from
@@ -24,19 +24,26 @@ def unzip(source_filename, dest_dir):
 
 arcpy.env.overwriteOutput = True
 
-inzip = arcpy.GetParameter(0) #r'C:\Users\Yogesh\Desktop\temp\states_21basic.zip'
-file_type= arcpy.GetParameter(1)
-shutil.rmtree(arcpy.env.scratchFolder)
+inzip= r'C:\Users\Yogesh\Desktop\temp\states_21basic.zip'
+file_type = 'shapefile'
+print (inzip)
+sc=r'C:\Users\Yogesh\Documents\ArcGIS\scratch'
+print (sc)
 
-unzip(inzip,arcpy.env.scratchFolder)
+unzip(inzip,sc)
+
+workspace = sc
+
+
 fcs=[]
-for dirpath, dirnames, filenames in arcpy.da.Walk(arcpy.env.scratchFolder,
+for dirpath, dirnames, filenames in arcpy.da.Walk(workspace,
                                                   datatype="FeatureClass",
                                                   type="ALL"):
     for filename in filenames:
         arcpy.CopyFeatures_management(os.path.join(dirpath, filename),"in_memory/temp")
         fc=arcpy.FeatureSet()
         fc.load("in_memory/temp")
+        print fc.JSON[1:300]
         fcs.append(fc)
 
 arcpy.SetParameter(2,fcs)
